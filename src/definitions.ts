2f52a18e156a40cdc8ec1f9c7b52f598d148e078
Jille <jille.van.der.weerd@beeproger.com>
2019-02-05 14:15:29 +0100

    Adds typings

--- src/definitions.ts
+++ src/definitions.ts
@@ -6,4 +6,8 @@ declare global {
 
 export interface CapacitorGeofencingPlugin {
   echo(options: { value: string }): Promise<{value: string}>;
+  setup(options: { url: string, notifyOnEntry: boolean, notifyOnExit: boolean, payload: object }): Promise<{value: string}>;
+  addRegion(options: { latitude: number, longitude: number, identifier: string }): Promise<{value: string}>;
+  stopMonitoring(options: { identifier: string }): Promise<{value: string}>;
+  monitoredRegions(): Promise<{value: string}>;
 }
