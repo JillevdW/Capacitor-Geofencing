78687a83c3baf5f2f68fe07441d94d123b40f634
Jille <jille.van.der.weerd@beeproger.com>
2019-02-05 15:19:18 +0100

    Made radius param optional, version bump

--- src/definitions.ts
+++ src/definitions.ts
@@ -6,7 +6,7 @@ declare global {
 
 export interface CapacitorGeofencingPlugin {
   setup(options: { url: string, notifyOnEntry: boolean, notifyOnExit: boolean, payload: object }): Promise<{value: string}>;
-  addRegion(options: { latitude: number, longitude: number, identifier: string }): Promise<{value: string}>;
+  addRegion(options: { latitude: number, longitude: number, radius?: number, identifier: string }): Promise<{value: string}>;
   stopMonitoring(options: { identifier: string }): Promise<{value: string}>;
   monitoredRegions(): Promise<{value: string}>;
 }
